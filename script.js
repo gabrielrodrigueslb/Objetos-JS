aa0b01b211e5562f269edca7423bf657f0cff4a9
Gabriel <gbri2586@gmail.com>
2024-09-23 20:59:38 -0300

    Upload dos arquivos

---
+++ script.js
@@ -0,0 +1,35 @@
+const nome = 'O Senhor dos Anéis';
+const ano = 1954;
+const autor = 'J. R. R. Tolkien';
+
+
+const livro = {
+    nome: 'O Senhor dos Anéis',
+    ano: 1954,
+    autor:'J. R. R. Tolkien'
+}
+const filme = {
+    nome: 'O senhor dos Anéis',
+    ano: 2001,
+    autor:'J. R. R. Tolkien'
+}
+
+console.log(livro.nome === nome);
+
+const texto = document.querySelector("p")
+
+
+console.log(autor.toLowerCase())
+
+const decimal = 122.99
+
+console.log(decimal.toFixed())
+console.log(3)
+
+
+console.log(Number(decimal.toFixed()) + 3)
+
+
+const total = decimal.toFixed().length.toFixed();
+
+console.log(total)
